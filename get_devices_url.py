8f07ea54fe31e1e3b44f4f09ad4e62c7d70b78fa
pzim <52496172+pzim-devdata@users.noreply.github.com>
2023-05-11 18:10:28 +0200

    Add Sunscreens, Scenarios, Sensors, Open to specific level

--- get_devices_url.py
+++ get_devices_url.py
@@ -20,6 +20,7 @@ from pyoverkiz.models import Scenario
 async def main() -> None:
 
     passwd_file = os.path.dirname(os.path.abspath(__file__))+'/temp/identifier_file.txt'
+
     list_of_tahoma_devices = os.path.dirname(os.path.abspath(__file__))+'/temp/list_of_tahoma_devices.txt'
     list_of_tahoma_shutters = os.path.dirname(os.path.abspath(__file__))+'/temp/shutters.txt'
     list_of_tahoma_heaters = os.path.dirname(os.path.abspath(__file__))+'/temp/heaters.txt'
@@ -28,6 +29,8 @@ async def main() -> None:
     list_of_tahoma_plugs = os.path.dirname(os.path.abspath(__file__))+'/temp/plugs.txt'
     list_of_tahoma_sunscreens = os.path.dirname(os.path.abspath(__file__))+'/temp/sunscreens.txt'
     list_of_tahoma_scenes = os.path.dirname(os.path.abspath(__file__))+'/temp/scenarios.txt'
+    list_of_tahoma_sensors = os.path.dirname(os.path.abspath(__file__))+'/temp/sensors.txt'
+    list_of_tahoma_states = os.path.dirname(os.path.abspath(__file__))+'/temp/states.txt'
 
     server_choosen =  os.path.dirname(os.path.abspath(__file__))+'/temp/server_choosen.txt'
 
@@ -46,6 +49,8 @@ async def main() -> None:
     f7 = open(list_of_tahoma_plugs, 'w')
     f8 = open(list_of_tahoma_sunscreens, 'w')
     f9 = open(list_of_tahoma_scenes, 'w')
+    f10 = open(list_of_tahoma_sensors, 'w')
+    f11 = open(list_of_tahoma_states, 'w')
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--username")
@@ -79,34 +84,88 @@ async def main() -> None:
             return
         devices = await client.get_devices()
         scenarios = await client.get_scenarios()
-    try :
-        f2.write(f"Devices :\n")
-        for device in devices:
-            print(f"\n{device.label},{device.id},{device.widget}")
-            f2.write(f"{device.label},{device.id},{device.widget},{device.ui_class},{device.controllable_name}\n")
-            if "Shutter" in device.widget or "PositionableTiltedScreen" in device.widget:
-                f3.write(device.label+","+device.id+","+device.widget+"\n")
-                print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_shutters)
-            elif "Heater" in device.widget :
-                f4.write(device.label+","+device.id+","+device.widget+"\n")
-                print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_heaters)
-            elif "MyFoxAlarm" in device.widget :
-                f5.write(device.label+","+device.id+","+device.widget+"\n")
-                print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_alarms)
-            elif "StatefulOnOffLight" in device.widget :
-                f6.write(device.label+","+device.id+","+device.widget+"\n")
-                print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_spotalarms)
-            elif "StatelessOnOff" in device.widget :
-                f7.write(device.label+","+device.id+","+device.widget+"\n")
-                print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_plugs)
-            elif "PositionableScreen" in device.widget or "PositionableHorizontalAwning" in device.widget:
-                f8.write(device.label+","+device.id+","+device.widget+"\n")
-                print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_sunscreens)
-            else :
-                print( "Device '"+device.label+"' NOT controlled by tahoma yet")
-    except Exception as e :
-        print(e)
-    print("\nScenes :\n")
+        try :
+            f2.write(f"Devices :\n")
+            for device in devices:
+                print("\n"+device.label+","+device.id+","+device.widget+","+device.ui_class+","+device.controllable_name+"")
+                print(f"{device.label},{device.id},{device.widget},{device.ui_class},{device.controllable_name}")
+                f2.write("\n"+device.label+","+device.id+","+device.widget+","+device.ui_class+","+device.controllable_name+"\n")
+                f2.write(f"{device.label},{device.id},{device.widget},{device.ui_class},{device.controllable_name}\n")
+                if "Shutter" in device.widget or "PositionableTiltedScreen" in device.widget:
+                    f3.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_shutters)
+                elif "Heater" in device.widget :
+                    f4.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_heaters)
+                elif "MyFoxAlarm" in device.widget :
+                    f5.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_alarms)
+                elif "StatefulOnOffLight" in device.widget :
+                    f6.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_spotalarms)
+                elif "StatelessOnOff" in device.widget :
+                    f7.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_plugs)
+                elif "PositionableScreen" in device.widget or "PositionableHorizontalAwning" in device.widget:
+                    f8.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_sunscreens)
+                elif "Sensor" in device.widget:
+                    f10.write(device.label+","+device.id+","+device.widget+"\n")
+                    print( "Device "+device.label+" controled by tahoma. Added to : "+list_of_tahoma_sensors)
+                else :
+                    print( "Device '"+device.label+"' NOT controlled by tahoma yet")
+                get_state = await client.get_state( device.id )
+                i=0
+                for i in range(len(get_state)) :
+    #                print(i)
+    #                print(str(get_state[i].value))
+                    if "closed" in str(get_state[i].value) or "open" in str(get_state[i].value) :
+                        f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['closed','open'],"+str(get_state[i].value)+"\n")
+                        print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                    elif "armed" in str(get_state[i].value) or "disarmed" in str(get_state[i].value) :
+                        f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['armed','disarmed'],"+str(get_state[i].value)+"\n")
+                        print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                    elif "Heater" not in device.widget :
+                        if 'DomesticHotWaterTank' not in device.widget :
+                            if str(get_state[i].value) == 'on' or str(get_state[i].value) =='off':
+                                f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['on','off'],"+str(get_state[i].value)+"\n")
+                                print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                    elif str(get_state[i].name) != 'io:TargetHeatingLevelState' :
+                        if str(get_state[i].name) != 'core:OnOffState':
+                            if str(get_state[i].value) == 'eco' or str(get_state[i].value) =='comfort' or str(get_state[i].value) =='frostprotection' or str(get_state[i].value) =='off':
+                                f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['eco','comfort','frostprotection','off'],"+str(get_state[i].value)+"\n")
+                                print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                    if 'TemperatureSensor' == device.widget :
+                        try:
+                            if str(get_state[i].name) == 'core:TemperatureState':
+                                f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['°C'],"+str(get_state[i].value)+"\n")
+                                print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                        except : pass
+                    if 'LuminanceSensor' == device.widget :
+                        #print(get_state)
+                        try:
+                            if str(get_state[i].name) == 'core:LuminanceState' :
+                            #if str(int(get_state[i].value)).isnumeric() == True :
+                                f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['Lumens'],"+str(get_state[i].value)+"\n")
+                                print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                        except : pass
+                    if 'CumulativeElectricPowerConsumptionSensor' == device.widget :
+                        try:
+                            if str(get_state[i].name) == 'core:ElectricEnergyConsumptionState' :
+                                f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['kWh'],"+str(get_state[i].value)+"\n")
+                                print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                        except : pass
+                    if 'DomesticHotWaterTank' == device.widget :
+                        try:
+                            if str(get_state[i].name) == 'io:ForceHeatingState' :
+                                f11.write(device.label+","+device.id+","+device.widget+",get_state["+str(i)+"].value,['on','off'],"+str(get_state[i].value)+"\n")
+                                print("States for "+device.label+" added to : "+ list_of_tahoma_states)
+                        except : pass
+
+                    i=i+1
+        except Exception as e :
+            print(e)
+        print("\nScenes :\n")
     try :
         f2.write(f"\nScenes :\n")
         for scenario in scenarios:
@@ -124,8 +183,10 @@ async def main() -> None:
     f7.close()
     f8.close()
     f9.close()
-    
-    print( "\nIf you want to add a device you have found in this list but which is not controlled by tahoma yet, please provide info about this device from this file at \nhttps://github.com/pzim-devdata/tahoma/issues and I will update the plugin ;-).")
+    f10.close()
+    f11.close()
+
+    print( "\nIf you want to add a device you have found in this list but which is not controlled by tahoma yet, please provide info about this device from this file at \nhttps://github.com/pzim-devdata/tahoma/issues and I will update the plugin ;-). \nSonos products can't be had, use 'soco-cli' instead")
     print( "\nThe list of devices has been succesfully imported to the file : "+list_of_tahoma_devices+"\n" )
 
 try:
@@ -135,6 +196,5 @@ except NameError as e:
     print(e)
     print("\nYou didn't specified any USERNAME or PASSWORD.\nExecute tahoma --config or provide a temporary USERNAME and PASSWORD by executing tahoma -u <USERNAME> -p <PASSWORD> command")
     exit(1)
-
 
 
