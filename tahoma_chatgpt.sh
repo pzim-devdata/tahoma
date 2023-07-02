fdc595734851b2a210cb8266c5f4efd3e469859a
pzim <52496172+pzim-devdata@users.noreply.github.com>
2023-07-02 17:50:48 +0200

    Update tahoma_chatgpt.sh

--- tahoma_chatgpt.sh
+++ tahoma_chatgpt.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #Vous pouvez donner votre instruction tahoma directement en argument de ce script : "~/Bureau/tahoma_chatgpt.sh ferme les volets de la cuisine"
 #You can give your Tahoma instruction directly as an argument to this script. "~/Desktop/tahoma_chatgpt.sh please close the kitchen shutters"
-bash
-Copy code
-~/Desktop/tahoma_chatgpt.sh close the kitchen shutters
+
 #$echo "$(dirname "$0")"
 #cd 'foler/of/tahoma-gpt';
 cd "$(dirname "$0")"
