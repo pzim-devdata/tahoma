9e47dfa62d75bcaca40cb2ce19fe0b2132394612
pzim <52496172+pzim-devdata@users.noreply.github.com>
2023-07-11 19:40:01 +0200

    Update tahoma_chatgpt.bat

--- tahoma_chatgpt.bat
+++ tahoma_chatgpt.bat
@@ -5,7 +5,8 @@ rem You can give your Tahoma instruction directly as an argument to this script.
 rem "C:\tahoma-gpt\tahoma_chatgpt.bat please close the kitchen shutters"
 
 rem Déplacement vers le dossier contenant le script
-cd /d "%~dp0"
+set curdir=%~dp0
+cd /d "%curdir%"
 
 rem Activation de l'environnement virtuel Python
 call env\Scripts\activate
@@ -14,10 +15,12 @@ rem Récupération des arguments
 set phrase=
 :loop
 if "%~1"=="" goto run
-set phrase=%phrase%%1 
+set phrase=%phrase% %1 
 shift
 goto loop
-
 :run
+
+echo %phrase%
+
 rem Lancement du script tahoma-gpt.py avec l'instruction Tahoma
-python tahoma-gpt.py "%phrase%"
+python tahoma-gpt.py %phrase%
