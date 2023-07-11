e44f8e628dc94b37d2e617af79ba47bfe08a520d
pzim <52496172+pzim-devdata@users.noreply.github.com>
2023-07-11 15:16:31 +0200

    Create tahoma_chatgpt.bat

---
+++ tahoma_chatgpt.bat
@@ -0,0 +1,23 @@
+@echo off
+rem Vous pouvez donner votre instruction Tahoma directement en argument de ce script :
+rem "C:\tahoma-gpt\tahoma_chatgpt.bat ferme les volets de la cuisine"
+rem You can give your Tahoma instruction directly as an argument to this script.
+rem "C:\tahoma-gpt\tahoma_chatgpt.bat please close the kitchen shutters"
+
+rem Déplacement vers le dossier contenant le script
+cd /d "%~dp0"
+
+rem Activation de l'environnement virtuel Python
+call env\Scripts\activate
+
+rem Récupération des arguments
+set phrase=
+:loop
+if "%~1"=="" goto run
+set phrase=%phrase%%1 
+shift
+goto loop
+
+:run
+rem Lancement du script tahoma-gpt.py avec l'instruction Tahoma
+python tahoma-gpt.py "%phrase%"
